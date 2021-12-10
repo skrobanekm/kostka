9d1fbb3d8bf2a86cb4033b9869be88ce26845b79
skrobanekm <matej208@seznam.cz>
2021-12-10 18:01:19 +0100

    second

--- js/game.js
+++ js/game.js
@@ -1,13 +1,18 @@
 const kostka = document.getElementById('kostka');
+const kostka2 = document.getElementById('kostka2');
 const statistika = document.getElementById('statistika');
 const tlacitko = document.getElementById('tlacitko');
 let hod = 1;
+let hod2 = 1;
 let hody = [];
+let hody2 = [];
 let timer = false;
 
 function animace(){
     hod = Math.ceil(Math.random() * 6);
     kostka.src = 'img/kostka' + hod + '.png';
+    hod2 = Math.ceil(Math.random() * 6);
+    kostka2.src = 'img/kostka' + hod2 + '.png';
 }
 
 tlacitko.addEventListener('click', function(){
@@ -19,19 +24,42 @@ tlacitko.addEventListener('click', function(){
         timer= false;
         tlacitko.innerText = 'HREJ';
         hody.push(hod);
+        hody2.push(hod2);
         vypisStatistiky();
     }
 })
 
+
 function suma(){
-    let sum = 0;
+    let sum1 = 0;
+    let sum2 = 0;
     for (let i =0; i < hody.length; i++){
-        sum += hody[i]
+        sum1 += hody[i]
+        sum2 += hody2[i]
+        
     } 
-    return sum;
+    return sum= sum1+sum2;
 }
 
-function max(){
+function suma1(){
+    let sum1 = 0;
+    for (let i =0; i < hody.length; i++){
+        sum1 += hody[i]
+        
+    } 
+    return sum1;
+}
+function suma2(){
+    let sum2 = 0;
+    for (let j =0; j < hody2.length; j++){
+        sum2 += hody2[j]
+        
+    } 
+    return sum2;
+}
+
+
+/*function max(){
     let maximum = 1
     hody.forEach(function(value,index){
         if (value > maximum) maximum = value;
@@ -46,14 +74,18 @@ function min(){
     })
     return minimum;
 }
+*/
 
 function vypisStatistiky(){
-    statistika.innerHTML =`<p>Poslední hod: ${hod}</p>`;
+    statistika.innerHTML =`<p>Poslední hod: ${hod}+${hod2}</p>`;
     statistika.innerHTML +=`<p>Počet hodů: ${hody.length}</p>`;
-    statistika.innerHTML +=`<p>Součet: ${suma()}</p>`;
+    statistika.innerHTML +=`<p>Součet 1. kostky: ${suma1()}</p>`;
+    statistika.innerHTML +=`<p>Součet 2. kostky: ${suma2()}</p>`;
+    statistika.innerHTML +=`<p>Celkový součet: ${suma()}</p>`;
     statistika.innerHTML +=`<p>Průměr: ${(suma() / hody.length).toFixed(2)}</p>`;
-    statistika.innerHTML +=`<p>Maximum: ${max()}</p>`;
-    statistika.innerHTML +=`<p>Minimum: ${min()}</p>`;
+
+    /*statistika.innerHTML +=`<p>Maximum: ${max()}</p>`;
+    statistika.innerHTML +=`<p>Minimum: ${min()}</p>`;*/
 }
 
 
